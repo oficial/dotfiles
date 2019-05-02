2706f177d9e63cb224bc240ef578b462ceff7067
Ricardo Wellington <oficial@gmail.com>
2019-05-02 14:39:09 -0300

    Atualização do script de build e do arquivo de configurações

--- vim/build.sh
+++ vim/build.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
+DIR=$(mktemp -d)
+
+cd $DIR
+
+git clone https://github.com/vim/vim.git
+
+cd vim
+
+mkdir -p ~/bin/vim
+
 ./configure \
-	--with-features=normal \
-	--prefix=/usr \
+    --with-features=normal \
+    --prefix=/home/ricardo/bin/vim \
+    --with-local-dir=/home/ricardo/bin/vim \
     --localstatedir=/var/lib/vim \
-	--enable-multibyte \
-	--enable-pythoninterp=yes \
-	--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+    --enable-multibyte \
+    --enable-pythoninterp=yes \
+    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
     --with-x=no \
     --disable-gui \
     --disable-netbeans \
     --enable-cscope \
     --enable-terminal=yes \
     --with-compiledby='Ricardo'
+
+make
+
+make install
 
 
