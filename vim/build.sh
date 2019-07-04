d1456034042d25cfde3587c25031ea667e75c47f
Ricardo Wellington <oficial@gmail.com>
2019-07-04 17:14:24 -0300

    Atualização do VIM para usar vim-plug

--- vim/build.sh
+++ vim/build.sh
@@ -7,21 +7,31 @@ git clone https://github.com/vim/vim.git
 
 cd vim
 
+rm -rf ~/bin/vim
+
 mkdir -p ~/bin/vim
 
 ./configure \
-    --with-features=normal \
-    --prefix=/home/ricardo/bin/vim \
+    --with-features=${1:-normal} \
+    --with-mac-arch=intel \
+    --with-x=no \
     --with-local-dir=/home/ricardo/bin/vim \
+    --prefix=/home/ricardo/bin/vim \
     --localstatedir=/var/lib/vim \
     --enable-multibyte \
     --enable-pythoninterp=yes \
     --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-    --with-x=no \
-    --disable-gui \
-    --disable-netbeans \
     --enable-cscope \
     --enable-terminal=yes \
+    --enable-signs \
+    --disable-gui \
+    --disable-netbeans \
+    --disable-darwin \
+    --disable-selinux \
+    --disable-arabic \
+    --disable-farsi \
+    --disable-rightleft \
+    --disable-gtktest \
     --with-compiledby='Ricardo'
 
 make
--- vim/plugins.vim
+++ vim/plugins.vim
@@ -1,4 +1,5 @@
 call plug#begin('~/.vim/plugged')
+Plug 'kshenoy/vim-signature'
 Plug 'kien/ctrlp.vim'
 Plug 'tomtom/tcomment_vim'
 Plug 'tpope/vim-fugitive'
